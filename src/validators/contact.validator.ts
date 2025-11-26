fbf9375ac7f45c0f586e4b023b30169777814d70
Lucadron <emregulsentr0@gmail.com>
2025-11-26 17:33:10 +0300

    fix(validator): subject alanı opsiyonel hale getirildi ve validation kaynaklı 400 hatası çözüldü

--- src/validators/contact.validator.ts
+++ src/validators/contact.validator.ts
@@ -3,8 +3,9 @@ import { z } from 'zod';
 export const createContactSchema = z.object({
     name: z.string().min(2),
     email: z.string().email(),
-    subject: z.string().min(3),
+    subject: z.string().optional(),
     message: z.string().min(10)
 });
 
 export type CreateContactInput = z.infer<typeof createContactSchema>;
+
