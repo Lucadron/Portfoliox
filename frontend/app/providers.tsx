e38e87c1720f9d512981a7502049cc47c95a73e7
Lucadron <emregulsentr0@gmail.com>
2025-11-27 20:11:35 +0300

    Fix: Lang toggle context fixed

--- frontend/app/providers.tsx
+++ frontend/app/providers.tsx
@@ -1,8 +1,16 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactNode, useState } from "react";
+import { LangProvider } from "@/context/LangContext";
 
 export default function Providers({ children }: { children: ReactNode }) {
     const [client] = useState(() => new QueryClient());
-    return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+
+    return (
+        <QueryClientProvider client={client}>
+            <LangProvider>
+                {children}
+            </LangProvider>
+        </QueryClientProvider>
+    );
 }
